55869f936f4aee32675d192669f2c2ecfda9d90c
Andrew Sousa <asousa2021@gmail.com>
2026-07-14 16:20:26 -0700

    Refactor model and training configuration
    Updated model architecture and training parameters.

--- main.py
+++ main.py
@@ -29,40 +29,51 @@ test_ds = tf.keras.utils.image_dataset_from_directory(
 )
 
 class_names = train_ds.class_names
+
 print(train_ds.class_names)
-
-normalization = tf.keras.layers.Rescaling(1./255)
-
-train_ds = train_ds.map(lambda x, y: (normalization(x), y))
-valid_ds = valid_ds.map(lambda x, y: (normalization(x), y))
-test_ds = test_ds.map(lambda x, y: (normalization(x), y))
 
 data_augmentation = tf.keras.Sequential([
     tf.keras.layers.RandomFlip("horizontal"),
-    tf.keras.layers.RandomRotation(0.1),
+    tf.keras.layers.RandomRotation(0.05),
     tf.keras.layers.RandomZoom(0.1),
 ])
 
-train_ds = train_ds.map(lambda x, y: (data_augmentation(x), y))
+train_ds = train_ds.map(lambda x,y:(data_augmentation(x),y))
+
+normalization = tf.keras.layers.Rescaling(1./255)
+
+train_ds = train_ds.map(lambda x,y:(normalization(x),y))
+valid_ds = valid_ds.map(lambda x,y:(normalization(x),y))
+test_ds = test_ds.map(lambda x,y:(normalization(x),y))
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(AUTOTUNE)
+valid_ds = valid_ds.cache().prefetch(AUTOTUNE)
+test_ds = test_ds.cache().prefetch(AUTOTUNE)
 
 print("Making Model...")
 model = tf.keras.Sequential([
     tf.keras.layers.Input(shape=(224, 224, 3)),
 
-    tf.keras.layers.Conv2D(8,3,activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-
-    tf.keras.layers.Conv2D(16,3,activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-
     tf.keras.layers.Conv2D(32,3,activation='relu'),
+    tf.keras.layers.BatchNormalization(),
     tf.keras.layers.MaxPooling2D(),
 
-    tf.keras.layers.Flatten(),
+    tf.keras.layers.Conv2D(64,3,activation='relu'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPooling2D(),
 
-    tf.keras.layers.Dense(32,activation='relu'),
-    tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(5,activation='softmax')
+    tf.keras.layers.Conv2D(128,3,activation='relu'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.MaxPooling2D(),
+
+    tf.keras.layers.GlobalAveragePooling2D(),
+
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Dropout(0.4),
+    tf.keras.layers.Dense(5, activation='softmax')
 ])
 print("Model complete")
 model.summary()
@@ -73,13 +84,20 @@ model.compile(
     metrics=['accuracy']
 )
 
+early_stop = tf.keras.callbacks.EarlyStopping(
+    monitor="val_accuracy",
+    patience=8,
+    restore_best_weights=True
+)
+
 history = model.fit(
     train_ds,
     validation_data=valid_ds,
-    epochs=20
+    epochs=50,
+    callbacks=[early_stop]
 )
 
-model.save("fruit-modal2.keras")
+model.save("fruit-modal6.keras")
 
 
 
